0ffde3857f68f7b9f788340df03360fc5b6fb823
tauri_s <tauristclaire@gmail.com>
2023-12-13 16:54:30 -0800

    Starter code and dependencies

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Tabs from "./Tabs";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>SqueakyClean Laundromat </h1>
+      <h2>We take the dirty work out of laundry!</h2>
+      <Tabs />
+    </div>
+  );
+}
