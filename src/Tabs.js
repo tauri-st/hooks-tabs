251fa7021f4ec3a2602c2b7dc037662bd7a3b590
tauri_s <tauristclaire@gmail.com>
2023-12-14 15:21:05 -0800

    Create state to show which tab is active

--- src/Tabs.js
+++ src/Tabs.js
@@ -3,6 +3,10 @@ import React, "{useState}" from "react";
 
 //3. Destructure services from the props object
 const Tabs = ({ services }) => {
+  /*5. In order to know which tab is visible, create state to 
+  keep track of which one is currently visible with initial
+  state being 0 place in the array */
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
   return (
     <section className="tabs">
       <div></div>
